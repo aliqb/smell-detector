2df1fc08ecb47d6ac208ec93c4a5701253a29fac
Ali Qayyoomi <aliqbhastam@gmail.com>
2024-02-14 13:54:57 +0330

    add lime and cleaning

--- SmellDetector.py
+++ SmellDetector.py
@@ -1,15 +1,19 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate, cross_val_predict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler
 from joblib import dump, load
 from sklearn.model_selection import GridSearchCV
+from lime import lime_tabular
+import matplotlib
+import matplotlib.pyplot as plt
+import re
+
+matplotlib.use('TkAgg')
 
 
 class SmellDetector:
@@ -22,6 +26,8 @@ class SmellDetector:
         self.y_train_balanced = None
         self.last_model = None
         # self.trained_model = None
+        # if isinstance(model, SVC):
+        #     model.probability = True  # Ensure probability is enabled for SVC
         self.model = model
         self.dataset_name = dataset_name
         self.scaling = scaling
@@ -51,7 +57,7 @@ class SmellDetector:
             except Exception as e:
                 print(f"Error in column {col}: {e}")
         nan_locs_X = np.where(X.isnull())
-        nan_indices_X = list(zip(nan_locs_X[0], nan_locs_X[1]))
+        # nan_indices_X = list(zip(nan_locs_X[0], nan_locs_X[1]))
 
         # Print out the indices of NaN values in X
         # for row, col in nan_indices_X:
@@ -63,8 +69,12 @@ class SmellDetector:
                                                                                 random_state=42)
         if self.scaling:
             scaler = MinMaxScaler()
-            self.X_train = scaler.fit_transform(self.X_train)
-            self.X_test = scaler.transform(self.X_test)
+            self.X_train = pd.DataFrame(scaler.fit_transform(self.X_train), columns=self.X_train.columns,
+                                        index=self.X_train.index)
+
+            # Transform the test data and convert back to DataFrame
+            self.X_test = pd.DataFrame(scaler.transform(self.X_test), columns=self.X_test.columns,
+                                       index=self.X_test.index)
         self.X_train_balanced, self.y_train_balanced = self.balance_dataset(self.X_train, self.y_train)
 
     def set_last_model(self):
@@ -122,6 +132,8 @@ class SmellDetector:
         scoring = ('accuracy', 'precision', 'recall', 'f1', 'roc_auc')
         # if self.param_grid is not None:
         self.model, best_params = self.find_best_model()
+        if isinstance(self.model, SVC):
+            self.model.probability = True  # Ensure probability is enabled for SVC
         print(f"best params: {best_params}")
         # else:
         #     best_model = self.model
@@ -131,14 +143,14 @@ class SmellDetector:
         print("##############################################")
         print(f"Model: {self.model.__class__.__name__}")
         print("cross validation result")
-        print(f"Accuracy: {cv_results['test_accuracy'].mean():.2f}")
-        print(f"Precision: {cv_results['test_precision'].mean():.2f}")
-        print(f"Recall: {cv_results['test_recall'].mean():.2f}")
-        print(f"F1-Score: {cv_results['test_f1'].mean():.2f}")
-        print(f"roc_auc: {cv_results['test_roc_auc'].mean():.2f}")
+        print(f"Accuracy: {(cv_results['test_accuracy'].mean()*100):.2f}")
+        print(f"roc_auc: {(cv_results['test_roc_auc'].mean()*100):.2f}")
+        print(f"F1-Score: {(cv_results['test_f1'].mean()*100):.2f}")
+        print(f"Recall: {(cv_results['test_recall'].mean()*100):.2f}")
+        print(f"Precision: {(cv_results['test_precision'].mean()*100):.2f}")
 
         # self.model.fit(self.X_train_balanced, self.y_train_balanced)
-        self.test_classifier()
+        # self.test_classifier()
 
     def test_classifier(self):
         print("-------------------------------------")
@@ -155,8 +167,66 @@ class SmellDetector:
         f1 = f1_score(self.y_test, y_pred)
         roc_auc = roc_auc_score(self.y_test, y_pred)
 
-        print(f'Accuracy: {accuracy:.2f}')
-        print(f'precision: {precision:.2f}')
-        print(f'recall: {recall:.2f}')
-        print(f'f1_score: {f1:.2f}')
-        print(f'roc_auc: {roc_auc:.2f}')
+        print(f'Accuracy: {(accuracy*100):.2f}')
+        print(f'recall: {(recall*100):.2f}')
+        print(f'precision: {(precision*100):.2f}')
+        print(f'f1_score: {(f1*100):.2f}')
+        print(f'roc_auc: {(roc_auc*100):.2f}')
+
+    def explain_instance(self, instance_index, num_features=10):
+        if not self.is_fitted:
+            print("there is no trained model, training start:")
+            self.train_classifier()
+            return
+        # Create a Lime explainer object
+        # Note: There are different explainers in LIME for different types of data (e.g., text, tabular, and images).
+        explainer = lime_tabular.LimeTabularExplainer(
+            training_data=np.array(self.X_train_balanced),
+            feature_names=self.X_train_balanced.columns.tolist(),
+            class_names=['Non-smelly', 'Smelly'],
+            mode='classification'
+        )
+
+        # Wrap the model's predict_proba to include feature names
+        def model_predict_proba(data):
+            # Convert the numpy array data back to DataFrame with feature names
+            df = pd.DataFrame(data, columns=self.X_train_balanced.columns)
+            # Use the original model's predict_proba on the DataFrame
+            return self.model.predict_proba(df)
+
+        # Explain a prediction
+        exp = explainer.explain_instance(
+            data_row=np.array(self.X_test.iloc[instance_index]),
+            predict_fn=model_predict_proba,  # Use the wrapped predict function
+            num_features=num_features
+        )
+
+        # Display the explanation
+        # exp.show_in_notebook(show_table=True)
+        # fig = exp.as_pyplot_figure()
+        # For non-notebook environments, you can use exp.as_pyplot_figure()
+        # plt.show(block=True)
+        # print(exp.as_list())
+        return exp.as_list()
+
+    def get_global_importance(self, num_features=10):
+        # Select a sample of instances to explain
+        np.random.seed(42)  # For reproducibility
+        # sample_indices = np.random.choice(self.X_test.shape[0], num_samples, replace=False)
+        feature_importances = {}
+
+        for idx in range(self.X_test.shape[0]):
+            exp_list = self.explain_instance(idx, num_features)
+            for feature, importance in exp_list:
+                match = re.search('(\d*\.?\d*\s*<=?)?\s*([A-Za-z_]*)\s*(\d*\.?\d*\s*[><]=?)?', feature)
+                main_feature_name = match.group(2)
+                if main_feature_name in feature_importances:
+                    feature_importances[main_feature_name].append(importance)
+                else:
+                    feature_importances[main_feature_name] = [importance]
+
+        # Aggregate the feature importances
+        aggregated_importances = {feature: np.mean(importances) for feature, importances in feature_importances.items()}
+        sorted_importances = sorted(aggregated_importances.items(), key=lambda x: x[1], reverse=True)
+        print(sorted_importances)
+        return sorted_importances
--- main.py
+++ main.py
@@ -1,103 +1,13 @@
-import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_validate, cross_val_predict
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier
-import numpy as np
+
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.svm import SVC
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import MinMaxScaler
-from joblib import dump, load
+
 
 from SmellDetector import SmellDetector
 
-# def read_data(dataset_name):
-#     data = pd.read_csv(f'datasets/{dataset_name}.csv')
-#     X = data.drop(['IDType', 'project', 'package', 'complextype', 'is_smell'],
-#                   axis=1)  # Features (remove the target column)
-#     y = data['is_smell']  # Target variable
-#     X.replace('?', -1, inplace=True)
-#     for col in X.columns:
-#         try:
-#             X[col] = pd.to_numeric(X[col], errors='coerce')
-#
-#         except Exception as e:
-#             print(f"Error in column {col}: {e}")
-#     return X, y
-#
-#
-# def balance_dataset(X, y):
-#     # Step 1: Initialize your classifier
-#     classifier = RandomForestClassifier()
-#
-#     # Step 2: Perform 3-fold cross-validation to get confidence scores
-#     confidence_scores = cross_val_predict(classifier, X, y, cv=3, method='predict_proba', n_jobs=-1)
-#
-#     # Assuming the classifier outputs probabilities and you're interested in the second class (smelly)
-#     confidences = confidence_scores[:, 0]  # Confidence of being smelly
-#
-#     # Step 3: Filter instances based on confidence threshold
-#     threshold = 0.95
-#
-#     # Create a mask for non-smelly instances with high confidence
-#     high_confidence_non_smelly_mask = (y == 0) & (confidences > threshold)
-#
-#     # Use the mask to filter out high-confidence non-smelly instances from the DataFrame
-#     retain_mask = (y != 0) | (confidences <= threshold)
-#     # Filter the dataset based on the mask
-#     if isinstance(X, pd.DataFrame):
-#         filtered_X = X[retain_mask].reset_index(drop=True)
-#     else:  # Assuming numpy array
-#         filtered_X = X[retain_mask]
-#
-#     if isinstance(y, pd.Series):
-#         filtered_y = y[retain_mask].reset_index(drop=True)
-#     else:  # Assuming numpy array
-#         filtered_y = y[retain_mask]
-#
-#     return filtered_X, filtered_y
-#     # filtered_df now contains your filtered dataset
-#
-#
-# def train_classifier(X, y, model, scaling=False):
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#     scoring = ('accuracy', 'precision', 'recall', 'f1', 'roc_auc')
-#     if scaling:
-#         scaler = MinMaxScaler()
-#         X_train = scaler.fit_transform(X_train)
-#         X_test = scaler.transform(X_test)
-#     X_train_balanced, y_train_balnced = balance_dataset(X_train, y_train)
-#     cv_results = cross_validate(model, X_train_balanced, y_train_balnced, cv=10, scoring=scoring)
-#     model_name = model.__class__.__name__
-#     print("##############################################")
-#     print(f"Model: {model_name}")
-#     print("cross validation result")
-#     print(f"Accuracy: {cv_results['test_accuracy'].mean():.2f}")
-#     print(f"Precision: {cv_results['test_precision'].mean():.2f}")
-#     print(f"Recall: {cv_results['test_recall'].mean():.2f}")
-#     print(f"F1-Score: {cv_results['test_f1'].mean():.2f}")
-#     print(f"roc_auc: {cv_results['test_roc_auc'].mean():.2f}")
-#
-#     model.fit(X_train_balanced, y_train_balnced)
-#     print("-------------------------------------")
-#     print("test result")
-#     y_pred = model.predict(X_test)
-#     accuracy = accuracy_score(y_test, y_pred)
-#     precision = precision_score(y_test, y_pred)
-#     recall = recall_score(y_test, y_pred)
-#     f1 = f1_score(y_test, y_pred)
-#     roc_auc = roc_auc_score(y_test, y_pred)
-#
-#     print(f'Accuracy: {accuracy:.2f}')
-#     print(f'precision: {precision:.2f}')
-#     print(f'recall: {recall:.2f}')
-#     print(f'f1_score: {f1:.2f}')
-#     print(f'roc_auc: {roc_auc:.2f}')
 
-
-if __name__ == '__main__':
-    datasets = ['data-class', 'god-class', 'long-method', 'feature-envy']
+def run_cli(datasets, classifiers):
     print("Choose smell dataset")
     print("1: data class")
     print("2: god class")
@@ -107,18 +17,7 @@ if __name__ == '__main__':
     if 1 <= selected_dataset <= 4:
         dataset_name = datasets[selected_dataset - 1]
         print(f"dataset:{dataset_name}")
-        classifiers = [(DecisionTreeClassifier(), {'criterion': ['log_loss', 'gini'],
-                                                   'max_depth': list(range(1, 101)) + [None]}),
-                       (RandomForestClassifier(),
-                        {'criterion': ['log_loss', 'gini'], 'max_depth': list(range(1, 101, 10)),
-                         'n_estimators': list(range(1, 200, 20))}),
-                       (HistGradientBoostingClassifier(), {
-                           'max_depth': list(range(1, 101, 10)), 'max_iter': list(range(1, 200, 20))
-                       }),
-                       (SVC(kernel='rbf'), {
-                           'C': [float(x) for x in range(200, 2001, 200)],
-                           'gamma': [float(i) for i in range(0, 100, 10)] + ['scale', 'auto']
-                       })]
+
         print('Choose your classifier:')
         for index in range(len(classifiers)):
             print(f"{index + 1}: {classifiers[index][0].__class__.__name__}")
@@ -127,13 +26,43 @@ if __name__ == '__main__':
             (model, param_grid) = classifiers[selected_classifier - 1]
             scaling = str(model.__class__.__name__).startswith('SVC')
             smell_detector = SmellDetector(dataset_name, model, param_grid=param_grid, scaling=scaling)
-            commands = [smell_detector.train_classifier, smell_detector.test_classifier]
-            print("1: train a model")
-            print("2: test  model")
-            selected_command = int(input())
-            if 1 <= selected_command <= len(commands):
-                commands[selected_command-1]()
+            commands = [smell_detector.train_classifier, smell_detector.test_classifier,
+                        smell_detector.get_global_importance, 'exit']
+            exiting = False
+            while not exiting:
+                print("1: train a model")
+                print("2: test  model")
+                print("3: explain model")
+                print("4: exit")
+                selected_command_index = int(input())
+                if 1 <= selected_command_index <= len(commands):
+                    selected_command = commands[selected_command_index - 1]
+                    if selected_command == 'exit':
+                        exiting = True
+                        continue
+                    selected_command()
+                    # smell_detector.get_global_importance()
+                else:
+                    print("invalid input")
         else:
             print("invalid input")
     else:
         print("invalid input")
+
+
+if __name__ == '__main__':
+    datasets = ['data-class', 'god-class', 'long-method', 'feature-envy']
+    classifiers = [(DecisionTreeClassifier(), {'criterion': ['log_loss', 'gini'],
+                                               'max_depth': list(range(1, 101)) + [None]}),
+                   (RandomForestClassifier(),
+                    {'criterion': ['log_loss', 'gini'], 'max_depth': list(range(1, 101, 10)),
+                     'n_estimators': list(range(1, 200, 20))}),
+                   (HistGradientBoostingClassifier(), {
+                       'max_depth': list(range(1, 101, 10)), 'max_iter': list(range(1, 200, 20))
+                   }),
+                   (SVC(kernel='rbf'), {
+                       'C': [float(x) for x in range(200, 2001, 200)],
+                       'gamma': [float(i) for i in range(0, 100, 10)] + ['scale', 'auto']
+                   })]
+
+    run_cli(datasets, classifiers)
